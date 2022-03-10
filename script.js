f18432047298645b9ac5c70f26488827dc395ec8
emonhossen99 <mdxhamedemon@gmail.com>
2022-03-10 19:34:55 +0600

    last added

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const handleSearch = () => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => showArtists(data));
+    const artistContainer = elementById("artists");
+    artistContainer.innerHTML = '';
 };
 
 const showArtists = (data) => {
-  const artistContainer = elementById("artist");
+  const artistContainer = elementById("artists");
   data?.artists?.forEach((artist) => {
     const div = document.createElement("div");
     div.classList.add("artist-card");
@@ -37,31 +39,33 @@ const showArtists = (data) => {
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+ 
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
+    .then((res) => res.json())
     .then((data) => showAlbum(data));
-  const artistContainer = elementById("artists");
-  artistContainer.innerHTML = "";
+    const artistContainer = elementById("artists");
+    artistContainer.textContent = "";
 };
 
 const showAlbum = (data) => {
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  data?.album?.forEach((item) => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${item.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
     albumContainer.appendChild(div);
-  });
+  })
+ 
 };
